52e3695b59a60ddd059e20ef80cab0c509d877bd
Caleb Frieze <k_lubmusic@me.com>
2013-04-03 18:32:31 -0700

    Added Routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,8 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :profile_name, presence: true, 
-                                            uniqueness: true,
-                                            
+                                            uniqueness: true
+
                                         
     
 
